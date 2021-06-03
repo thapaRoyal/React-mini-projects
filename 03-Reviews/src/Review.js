7a3e9f87ca48691f193ed1349bb6a1249401f0a6
Royal <thaparoyal17@gmail.com>
2021-06-03 19:57:56 +0545

    prev and next person

--- 03-Reviews/src/Review.js
+++ 03-Reviews/src/Review.js
@@ -5,6 +5,30 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -18,10 +42,10 @@ const Review = () => {
       <p className="text">{text}</p>
       <div className="button-container">
         <button className="prev-btn">
-          <FaChevronLeft />
+          <FaChevronLeft onClick={prevPerson} />
         </button>
         <button className="next-btn">
-          <FaChevronRight />
+          <FaChevronRight onClick={nextPerson} />
         </button>
       </div>
       <button className="random-btn">Surprise me</button>
