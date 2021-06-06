0f55a8f74ec4c3fdaefbe9ffd1d8d0600325e802
Royal <thaparoyal17@gmail.com>
2021-06-06 08:43:10 +0545

    manual categories approach

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu section">
@@ -14,7 +19,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+        Breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
