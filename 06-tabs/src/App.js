5cbb978f04cf667fb7d8e47c36f65102a75743eb
Royal <thaparoyal17@gmail.com>
2021-06-07 11:24:20 +0545

    initial setup

---
+++ 06-tabs/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading..</h1>
+      </section>
+    );
+  }
+
+  return <h2>jobs</h2>;
+}
+
+export default App;
