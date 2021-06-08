a3fe343787d1549d59c082e98a08c8bd3a9581d1
Royal <thaparoyal17@gmail.com>
2021-06-08 10:11:02 +0545

    Slider First And Last, Autoslide

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -7,6 +7,23 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
