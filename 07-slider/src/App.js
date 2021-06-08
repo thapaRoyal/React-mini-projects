d06e1dcab390df1ea5e23653e930416d83bf21e3
Royal <thaparoyal17@gmail.com>
2021-06-08 09:58:53 +0545

    slider setup classes

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -17,8 +17,19 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
