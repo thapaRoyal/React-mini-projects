d64ea99158d3c0a1549ae616f1b829c2b256ca27
Royal <thaparoyal17@gmail.com>
2021-06-09 11:22:51 +0545

    lorem ipsum structure

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum ?</h3>
+      <form action="#" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+          possimus. Facilis impedit delectus dolorem ratione?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+          possimus. Facilis impedit delectus dolorem ratione?
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
