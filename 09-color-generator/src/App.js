072ea65273b9ed1b259697da44526facb48c0647
Royal <thaparoyal17@gmail.com>
2021-06-10 10:16:12 +0545

    get values error

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -10,6 +10,13 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      // console.log(error);
+    }
   };
 
   return (
@@ -24,6 +31,7 @@ function App() {
               setColor(e.target.value);
             }}
             placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             Submit
