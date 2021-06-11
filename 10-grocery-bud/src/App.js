5172f0f4ba09005fbf1924e00f5f9af81063d983
Royal <thaparoyal17@gmail.com>
2021-06-11 11:07:50 +0545

    clear items

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -23,6 +23,7 @@ function App() {
       // deal with edit
     } else {
       // show alert
+      showAlert(true, "success", "items added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -31,6 +32,11 @@ function App() {
 
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
   };
 
   return (
@@ -54,7 +60,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">Clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            Clear items
+          </button>
         </div>
       )}
     </section>
