2ba74846ecf83e393a182afd1c6d94052340e129
Royal <thaparoyal17@gmail.com>
2021-06-12 10:44:27 +0545

    using useRef approach for navbar height || completed

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -5,6 +5,18 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
@@ -19,12 +31,8 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
