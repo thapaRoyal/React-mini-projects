3a91b8a5c12d9e1dda72ef72d81f80e5ec549054
Royal <thaparoyal17@gmail.com>
2021-06-12 10:30:31 +0545

    navbar simple toggle

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,30 +4,38 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => {
+              setShowLinks(!showLinks);
+            }}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <ul className="social-icons">
-          {social.map((social) => {
-            const { id, url, icon } = social;
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
             return (
               <li key={id}>
                 <a href={url}>{icon}</a>
