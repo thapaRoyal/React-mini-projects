4cc164be72d06b4148de4b596a01c1690601a95d
Royal <thaparoyal17@gmail.com>
2021-06-13 10:23:08 +0545

    structure

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  return (
+    <aside className={`sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.mao((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
